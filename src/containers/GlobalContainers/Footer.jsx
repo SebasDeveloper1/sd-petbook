b562b08b09ded235cba4fb32a416262a58ffd9b5
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-03-09 11:26:18 -0500

    :sparkles: add footer section

--- src/containers/GlobalContainers/Footer.jsx
+++ src/containers/GlobalContainers/Footer.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import { FaGithub, FaLink } from 'react-icons/fa';
+import { Typography } from 'components/indexComponents';
 
 export function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col justify-center items-center w-full h-64 text-center bg-slate-800">
+      <div className="flex flex-col items-center gap-3 w-11/12 ">
+        <Typography
+          variant="span_lg"
+          value="PetBook ◉ @SebasDeveloper ◉ 2023"
+          styles="inline-block font text-2xl tracking-tight text-blue-500 capitalize"
+        />
+        <Typography
+          variant="span_base"
+          value="Hecho con el 💙 por Sebastian Pedroza"
+          styles="inline-block font-medium text-slate-50"
+        />
+        <div className="flex space-x-8 w-fit mt-8 text-3xl text-slate-400">
+          <a
+            href="https://github.com/SebasDeveloper1/sd-petbook"
+            title="Github repository"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-slate-50"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://linktr.ee/sebasdeveloperoficial"
+            title="@SebasDeveloper contact"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-slate-50"
+          >
+            <FaLink />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
