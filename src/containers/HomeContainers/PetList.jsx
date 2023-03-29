7f5d593b7c60e1b96ec9230160daca07ba544fdb
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-03-29 15:17:16 -0500

    :lipstick: update UI(HomePage)

--- src/containers/HomeContainers/PetList.jsx
+++ src/containers/HomeContainers/PetList.jsx
@@ -7,7 +7,7 @@ export function PetList({ children } = {}) {
       <Typography
         variant="h2"
         value="Mis mascotas"
-        styles="relative border-b pt-12 pb-4 max-w-prose whitespace-nowrap text-2xl font-semibold text-slate-900 truncate after:absolute after:top-0 after:left-1/2 after:transform after:-translate-x-1/2 after:w-24 after:h-1 after:bg-cover after:bg-sky-500"
+        styles="relative border-b pt-12 pb-4 max-w-prose whitespace-nowrap text-2xl font-semibold text-slate-900 truncate after:absolute after:top-0 after:left-1/2 after:transform after:-translate-x-1/2 after:w-32 after:h-1 after:rounded-full after:bg-cover after:bg-sky-400"
       />
 
       <ul className="mt-14 mx-auto grid grid-cols-1 gap-6 w-full sm:gap-8 md:grid-cols-2 lg:grid-cols-3">
