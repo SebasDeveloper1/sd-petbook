614428472856c4fddd47a8376f4fdf43a12aa747
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2023-10-23 16:57:08 -0500

    :lipstick: Update LoadingSkeletonPetView

--- src/containers/PetViewContainers/LoadingSkeletonPetView.jsx
+++ src/containers/PetViewContainers/LoadingSkeletonPetView.jsx
@@ -1,67 +1,89 @@
 import React from 'react';
 import { Typography } from 'components/indexComponents';
 
+function LoadingSkeleton() {
+  return (
+    <div className="animate-pulse">
+      <div className="bg-gray-200 rounded-full w-60 h-60 mb-4" />
+      <div className="w-full mb-1 h-6 bg-gray-200 rounded-full" />
+      <div className="w-1/2 h-4 mx-auto bg-gray-200 rounded-full" />
+    </div>
+  );
+}
+
+function LoadingList() {
+  return (
+    <ul className="flex flex-col gap-6 p-4 text-xl text-slate-300">
+      {[1, 2, 3, 4, 5].map((key) => (
+        <li key={key} className="flex items-center gap-3 animate-pulse">
+          <div className="bg-gray-200 rounded-full w-8 h-8" />
+          <div className="w-2/3 h-4 bg-gray-200 rounded-full" />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function LoadingImage() {
+  return (
+    <div className="animate-pulse bg-gray-300 rounded-lg w-full aspect-square" />
+  );
+}
+
+function LoadingArticle() {
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <div className="animate-pulse bg-gray-300 rounded-lg h-8 w-full md:w-1/2" />
+      <div className="animate-pulse bg-gray-300 rounded-lg h-8 w-full md:w-2/3" />
+      <div className="animate-pulse bg-gray-300 rounded-lg h-8 w-full md:w-1/2" />
+    </div>
+  );
+}
+
+function LoadingListItem() {
+  return (
+    <li className="flex flex-col bg-gray-200 col-span-2 md:col-span-1 h-28 rounded-lg" />
+  );
+}
+
 export function LoadingSkeletonPetView() {
   return (
-    <div>
-      <section className="flex flex-col md:flex-row w-full min-h-screen bg-slate-900">
-        <div className="flex flex-col items-center w-full sm:1/2 lg:w-2/4 my-9">
-          <div className="w-11/12 space-y-12">
-            <div className="flex items-center gap-3">
-              <div className="w-14 h-14 rounded-full bg-gray-700 animate-pulse" />
-              <div className="z-10 relative w-full">
-                <div className="w-full h-8 rounded bg-gray-700" />
-                <div className="w-1/2 h-6 mt-1 rounded bg-gray-700 animate-pulse" />
-              </div>
-            </div>
-            <div className="h-60 space-y-6 p-6 rounded-lg border border-slate-800 bg-gray-700 animate-pulse" />
-          </div>
-        </div>
-        <div className="w-full lg:w-2/4 h-60 md:h-screen bg-gray-700 animate-pulse" />
-      </section>
-      <section className="pt-12 pb-24">
-        <div className="w-11/12 m-auto">
-          <Typography
-            variant="h3"
-            value="Historial de Vacunación"
-            styles="relative border-b pt-12 pb-4 max-w-prose font-semibold text-slate-900 after:absolute after:top-0 after:left-1/2 after:transform after:-translate-x-1/2 after:w-32 after:h-1 after:rounded-full after:bg-cover after:bg-sky-400"
-          />
-          <ul className="mt-12 mx-auto grid grid-cols-1 gap-6 w-full sm:gap-8 sm:grid-cols-2 md:col-span-3 lg:col-span-4">
-            <li className="w-full h-40 rounded bg-slate-200 animate-pulse" />
-            <li className="w-full h-40 rounded bg-slate-200 animate-pulse" />
-          </ul>
-        </div>
-      </section>
-      <section className="w-full pt-12 pb-24 bg-slate-900">
-        <div className="flex flex-col justify-center items-center w-11/12 m-auto">
-          <div className="relative max-w-xl mb-8">
-            {/* Loading skeleton for Typography component */}
-            <div className="animate-pulse bg-gray-300 rounded-lg h-16 w-full" />
-            <svg
-              aria-hidden="true"
-              viewBox="0 0 418 42"
-              className="absolute bottom-7 left-0 h-[2em] w-full fill-sky-400"
-              preserveAspectRatio="none"
-            >
-              {/* Loading skeleton for path element */}
-              <rect x="0" y="0" width="100%" height="100%" />
-            </svg>
-          </div>
-          <div className="grid grid-cols-3 justify-center items-center gap-6 w-full max-w-2xl px-6 py-8 rounded-xl bg-slate-800">
-            <figure className="col-span-3 md:col-span-1 ">
-              {/* Loading skeleton for image */}
-              <div className="animate-pulse bg-gray-600 rounded-lg h-44 w-44" />
-            </figure>
-
-            <section className="col-span-3 md:col-span-2 space-y-3 text-blue-50 text-xl">
-              {/* Loading skeleton for article */}
-              <div className="animate-pulse bg-gray-600 rounded-lg h-8 w-1/2" />
-              <div className="animate-pulse bg-gray-600 rounded-lg h-8 w-2/3" />
-              <div className="animate-pulse bg-gray-600 rounded-lg h-8 w-1/2" />
+    <section className="flex justify-center w-full min-h-screen py-12 bg-BeamsCover bg-contain bg-top bg-no-repeat">
+      <div className="w-11/12">
+        <section className="grid grid-cols-3 justify-center gap-6 divide-y md:divide-x md:divide-y-0">
+          <div className="col-span-3 md:col-span-1 flex flex-col items-center gap-6">
+            <LoadingSkeleton />
+            <div className="w-11/12 h-12 rounded-xl font-semibold bg-gray-200 animate-pulse" />
+            <section className="w-full pt-4 border-t">
+              <LoadingList />
             </section>
           </div>
-        </div>
-      </section>
-    </div>
+          <div className="col-span-3 md:col-span-2 w-full">
+            <div className="grid grid-cols-3 justify-center items-center gap-6 w-full p-6 max-w-2xl mx-auto mb-12 rounded-xl bg-gray-100">
+              <figure className="col-span-3 md:col-span-1 ">
+                {/* Loading skeleton for image */}
+                <LoadingImage />
+              </figure>
+              <section className="col-span-3 md:col-span-2 space-y-3 text-gray-50 text-xl">
+                {/* Loading skeleton for article */}
+                <LoadingArticle />
+              </section>
+            </div>
+            <div className="px-0 md:px-6 pt-6 pb-12 w-full">
+              <Typography
+                variant="h4"
+                value="Historial de Vacunación"
+                styles="pb-8 max-w-prose font-semibold text-slate-900 truncate"
+              />
+              <ul className="animate-pulse w-full mx-auto grid grid-cols-2 gap-3 md:gap-6">
+                {[1, 2, 3, 4].map((key) => (
+                  <LoadingListItem key={key} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
   );
 }
